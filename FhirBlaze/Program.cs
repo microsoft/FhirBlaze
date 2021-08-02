09a9f325bb102078bc128bae1d3720316bf7220c
Phil Jirsa <phil.jirsa@microsoft.com>
2021-08-02 15:20:52 -0500

    Add shared components and patient module

--- FhirBlaze/Program.cs
+++ FhirBlaze/Program.cs
@@ -15,14 +15,9 @@ namespace FhirBlaze
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-            builder.Services.AddMsalAuthentication(options =>
-            {
-                builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
-            });
 
             await builder.Build().RunAsync();
         }
