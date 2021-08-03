103d5df536bc760b43846a6304859879227ccf09
Sameer Doshi <sadoshi@microsoft.com>
2021-08-03 14:15:47 -0500

    Adding Patient List changes

---
+++ FhirBlaze.PatientModule/FHIRService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FhirBlaze.PatientModule
+{
+    public class FHIRService
+    {
+        public FHIRService(string endpoint)
+        {
+            
+        }
+    }
+}
---
+++ FhirBlaze.PatientModule/PatientList.razor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FhirBlaze.PatientModule
+{
+    public partial class PatientList
+    {
+        protected bool ShowCreate { get; set; } = false;
+
+        
+        public void ToggleCreate()
+        {
+            ShowCreate = !ShowCreate;
+            ShouldRender();
+        }
+    }
+}
