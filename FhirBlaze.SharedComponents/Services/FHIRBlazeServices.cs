774316bee383a025681239d689c4607fee3e4a77
Sameer Doshi <sadoshi@microsoft.com>
2021-08-04 08:38:07 -0500

    Adding no token exception logic to get requests

--- FhirBlaze.SharedComponents/Services/FHIRBlazeServices.cs
+++ FhirBlaze.SharedComponents/Services/FHIRBlazeServices.cs
@@ -1,5 +1,6 @@
 ﻿using Hl7.Fhir.Model;
 using Hl7.Fhir.Serialization;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
@@ -22,16 +23,30 @@ namespace FhirBlaze.SharedComponents.Services
       
         public async Task<IList<Patient>>  GetPatientsAsync()
         {
-            var r=await _client.GetAsync("/Patient");
-            var js=await r.Content.ReadAsStringAsync();
-            
-            var bundle=_parser.Parse<Bundle>(js);
+            string json = await DoGetAsync("/Patient");           
+            var bundle=_parser.Parse<Bundle>(json);
             var ret = new List<Patient>();
             foreach (var item in bundle.Entry)
             {
                 ret.Add((Patient)item.Resource);
             }
             return ret;
+        }
+
+        public async Task<string> DoGetAsync(string path)
+        {
+            string json = "{}";
+            try
+            {
+                var r = await _client.GetAsync(path);
+                json= await r.Content.ReadAsStringAsync();
+
+            }
+            catch (AccessTokenNotAvailableException exception)
+            {
+                exception.Redirect();
+            }
+            return json;
         }
     }
 }
