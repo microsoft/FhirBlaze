8236661e0334c962b87a0a2476285f55a4eb7646
i-am-dan <dakim@microsoft.com>
2021-08-06 15:15:29 -0400

    added rendercomponent

--- FhirBlaze.QuestionnaireModule/CreateQuestionnaire.razor.cs
+++ FhirBlaze.QuestionnaireModule/CreateQuestionnaire.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Hl7.Fhir.Model;
+using Microsoft.AspNetCore.Authorization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,20 +8,64 @@ using System.Threading.Tasks;
 
 namespace FhirBlaze.QuestionnaireModule
 {
+    [Authorize]
     public partial class CreateQuestionnaire
     {
         public Questionnaire Questionnaire { get; set; } = new Questionnaire();
 
-        public HumanName PatientName { get; set; } = new HumanName();
-       
+        protected Questionnaire.ItemComponent ItemComponenet { get; set; }
+
+        protected IList<ItemDisplay> ItemDisplays { get; set; }
+
+        protected IList<Questionnaire.ItemComponent> NewQuestionnaireItems { get; set; } = new List<Questionnaire.ItemComponent>();
+
         public int BedResponse { get; set; }
         public int TVResponse { get; set; }
         public int ACResponse { get; set; }
         public int BathroomResponse { get; set; }
 
-        protected void CreatePatient()
+        protected async void SaveQuestionnaire()
         {
-            
+
+        }
+
+        protected void AddItem()
+        {
+            ItemDisplays.Add(new ItemDisplay());
+        }
+
+        protected void AddStringItem()
+        {
+            NewQuestionnaireItems.Add(new Questionnaire.ItemComponent()
+            {
+                LinkId = new Guid().ToString(),
+                Type = Questionnaire.QuestionnaireItemType.String,
+                Text = ""
+            });
+        }
+        protected void AddBooleanItem()
+        {
+            NewQuestionnaireItems.Add(new Questionnaire.ItemComponent()
+            {
+                LinkId = new Guid().ToString(),
+                Type = Questionnaire.QuestionnaireItemType.Boolean,
+                Text = ""
+            });
+        }
+
+        protected void AddGroupItem()
+        {
+            NewQuestionnaireItems.Add(new Questionnaire.ItemComponent()
+            {
+                LinkId = new Guid().ToString(),
+                Type = Questionnaire.QuestionnaireItemType.Group,
+                Text = ""
+            });
+        }
+
+        protected void AddBooleanQuestion()
+        {
+
         }
     }
 }
