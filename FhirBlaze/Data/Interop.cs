01415c898f32f3c1de1923f186c5c9608854bfce
Alejandra Angulo Rico <43595879+Alangulo@users.noreply.github.com>
2021-08-11 03:39:05 -0500

    adding Power BI

---
+++ FhirBlaze/Data/Interop.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FhirBlaze.Data
+{
+    public static class Interop
+    {
+
+
+        internal static ValueTask<object> CreateReport(
+             IJSRuntime jsRuntime,
+             ElementReference reportContainer,
+             string accessToken,
+             string embedUrl,
+             string embedReportId)
+        {
+            return jsRuntime.InvokeAsync<object>(
+                "ShowMyPowerBI.showReport",
+                reportContainer, accessToken, embedUrl,
+                embedReportId);
+        }
+
+    }
+}
