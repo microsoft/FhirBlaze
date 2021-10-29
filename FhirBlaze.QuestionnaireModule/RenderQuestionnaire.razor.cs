c89f208539fc45c26f1cb569867e5c322a95d2d1
Phil Jirsa <phil.jirsa@microsoft.com>
2021-10-29 13:53:20 -0500

    add basic input control rendering for questionnaire

--- FhirBlaze.QuestionnaireModule/RenderQuestionnaire.razor.cs
+++ FhirBlaze.QuestionnaireModule/RenderQuestionnaire.razor.cs
@@ -1,15 +1,33 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using FhirBlaze.SharedComponents.Services;
+using Hl7.Fhir.Model;
+using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
 
 namespace FhirBlaze.QuestionnaireModule
 {
     public partial class RenderQuestionnaire
     {
+        [Inject]
+        public IFhirService FhirService { get; set; } 
         [Parameter]
         public string Id { get; set; }
+        public Questionnaire Questionnaire { get; set; } = new Questionnaire();
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                Questionnaire = await FhirService.GetQuestionnaireByIdAsync(Id);
+            }
+            catch (Exception)
+            {
+                // do error handling things here
+            }
+            ShouldRender();
+        }
     }
 }
