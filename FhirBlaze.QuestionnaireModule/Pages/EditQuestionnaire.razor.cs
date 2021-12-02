20b2422d5334a98754eca0e40d6d958e89ddc16c
DKim <wwjdat2000@yahoo.com>
2021-12-02 13:13:07 -0500

    trying to get update working

--- FhirBlaze.QuestionnaireModule/Pages/EditQuestionnaire.razor.cs
+++ FhirBlaze.QuestionnaireModule/Pages/EditQuestionnaire.razor.cs
@@ -19,18 +19,14 @@ namespace FhirBlaze.QuestionnaireModule.Pages
         public IFhirService FhirService { get; set; }
         [Inject]
         public HttpClient Http { get; set; }
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
         [Parameter]
         public string Id { get; set; }
         [Parameter]
         public EventCallback<Questionnaire> SaveQuestionnaire { get; set; }
         public bool QLoaded { get; set; } = false;
         public Questionnaire Questionnaire { get; set; } = new Questionnaire();
-        public QuestionnaireResponse QResponse { get; set; } = new QuestionnaireResponse();
-        public IList<QuestionnaireResponse> QuestionnaireResponses { get; set; } = new List<QuestionnaireResponse>();
-        public Dictionary<String, List<Coding>> AnswerOptionsDictionary { get; set; } = new Dictionary<string, List<Coding>>();
-        public Dictionary<String, Questionnaire.QuestionnaireItemType> QuestionTypesDictionary { get; set; } = new Dictionary<string, Questionnaire.QuestionnaireItemType>();
-        public string SubmittedMessage { get; set; }
-
 
         protected override async Task OnInitializedAsync()
         {
@@ -42,54 +38,8 @@ namespace FhirBlaze.QuestionnaireModule.Pages
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
-                // do error handling things here
             }
-        }
-
-        protected Questionnaire GenerateQR(Questionnaire Q)
-        {
-            Questionnaire q = new Questionnaire();
-          
-            foreach (var item in Questionnaire.Item)
-            {
-                q.Item.Add(GetItem(item));
-            }
-            return q;
-        }
-  
-        protected Questionnaire.ItemComponent GetItem(Questionnaire.ItemComponent qItem)
-        {
-            var Item = new Questionnaire.ItemComponent();
-            Item.Text = qItem.Text;
-            Item.LinkId = qItem.LinkId;
-            QuestionTypesDictionary.Add(qItem.LinkId, (Questionnaire.QuestionnaireItemType)qItem.Type);
-            if(qItem.Type == Questionnaire.QuestionnaireItemType.Group)
-            { 
-                Item.Item = GetItems(qItem.Item);
-            }
-            
-            return Item;
-        }
-
-        protected List<Questionnaire.ItemComponent> GetItems(List<Questionnaire.ItemComponent> qItems)
-        {
-            var Items = new List<Questionnaire.ItemComponent>();
-            foreach (var item in qItems)
-            {
-                switch (item.Type)
-                {
-
-                    case Questionnaire.QuestionnaireItemType.Group:
-                        Items = GetItems(item.Item);
-                        break;
-                    default:
-                        Items.Add(GetItem(item));
-                        break;
-                }
-            }
-
-            return Items;
-        }
+        }       
 
         protected void AddItem(QuestionnaireItemType type)
         {
@@ -107,11 +57,15 @@ namespace FhirBlaze.QuestionnaireModule.Pages
         {
             return (item.Type.Equals(Questionnaire.QuestionnaireItemType.Group)) ? "Group" : $"{item.Type.ToString()} Question";
         }
-        public async void Submit()
+        public async Task Submit()
         {
-            Questionnaire.Status = PublicationStatus.Draft;
-            await SaveQuestionnaire.InvokeAsync(Questionnaire);
+            if (!string.IsNullOrEmpty(Id))
+            {
+                Questionnaire.Status = PublicationStatus.Draft;
+                var questionnaire = await FhirService.CreateQuestionnaireAsync(Questionnaire);
+                NavigationManager.NavigateTo("/questionnaire");
+                Console.WriteLine($"Saved!  ID: {questionnaire.Id}");
+            }
         }
-
     }
 }
