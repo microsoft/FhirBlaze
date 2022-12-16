ce9a1ea0a676b24b44d79861a8b820e3cd82f3dc
Phil Jirsa <phil.jirsa@microsoft.com>
2022-12-16 10:12:31 -0600

    add compoenent folder

---
+++ FhirBlaze.PatientModule/Components/PatientSearch.razor.cs
@@ -0,0 +1,30 @@
+﻿using Hl7.Fhir.Model;
+using Microsoft.AspNetCore.Components;
+using System;
+
+namespace FhirBlaze.PatientModule.Components;
+
+public partial class PatientSearch
+{
+    [Parameter]
+    public EventCallback<Patient> SearchPatient { get; set; } 
+
+    [Parameter]
+    public bool Processing { get; set; }
+
+    [Parameter]
+    public models.SimplePatient Patient { get; set; }
+
+    protected async void searchPatient()
+    {
+        try
+        {
+           await SearchPatient.InvokeAsync(Patient.ToHl7Patient());
+        }
+        catch (Exception)
+        {
+            // do nothing
+        }
+    }
+
+}
